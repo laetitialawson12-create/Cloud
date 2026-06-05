92856632f03dba28539bfbdfb294c4b16cb1389f
Titia <laetitialawson12@gmail.com>
2026-06-05 19:13:41 +0000

    Changement 5

--- back-end/main.py
+++ back-end/main.py
@@ -5,9 +5,9 @@ from pydantic import BaseModel
 from typing import List
 import uvicorn
 
-app = FastAPI(title="API To-Do List Collaborative - Groupe 12")
+app = FastAPI(title="API To-Do List Collaborative")
 
-# Configuration CORS complète
+# Configuration CORS pour connecter votre HTML à Python sans blocage
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
@@ -20,12 +20,11 @@ class Task(BaseModel):
     nom: str
     texte: str
 
-# Base de données temporaire en mémoire RAM
+# Liste partagée stockée en mémoire RAM
 base_de_donnees_taches: List[Task] = []
 
-# Ajout du paramètre pour gérer automatiquement le slash final (ex: /taches et /taches/)
-@app.get("/taches", response_model=List[Task])
-@app.get("/taches/", response_model=List[Task], include_in_schema=False)
+@app.get("/taches")
+@app.get("/taches/", include_in_schema=False)
 def lister_taches():
     return base_de_donnees_taches
 
@@ -41,9 +40,7 @@ def supprimer_tache(index: int):
     if 0 <= index < len(base_de_donnees_taches):
         base_de_donnees_taches.pop(index)
         return {"status": "success"}
-    else:
-        raise HTTPException(status_code=404, detail="Tâche non trouvée")
+    raise HTTPException(status_code=404, detail="Non trouvé")
 
 if __name__ == "__main__":
-    # Force l'écoute sur le port exigé par OpenShift
     uvicorn.run(app, host="0.0.0.0", port=8080)
